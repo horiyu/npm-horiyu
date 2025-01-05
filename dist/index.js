a51b9e9b99526b33f0d6dd7250f6eeb631f4ed56
horiyu <horiyu.dev@gmail.com>
2025-01-06 02:38:13 +0900

    add: enhance CLI output with styled greeting and links

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
-"use strict";
-console.log(`\nhello, world\n`);
+import chalk from 'chalk';
+import boxen from 'boxen';
+const links = [
+    { label: 'Web', url: 'http://horiyu.com' },
+    { label: 'X', url: 'https://x.com/horiyu_com' },
+    { label: 'npm', url: 'https://www.npmjs.com/~horiyu_com' },
+    { label: 'Zenn', url: 'https://zenn.dev/horiyu' },
+    { label: 'GitHub', url: 'https://github.com/horiyu' },
+];
+const helloMessage = ' ' + chalk.bold('Hello, I am') + ' ' + chalk.green.bold.italic('horiyu') + ' ' + chalk.white('👋');
+const maxLabelLength = Math.max(...links.map(link => link.label.length));
+const linkMessages = links.map(link => {
+    const paddedLabel = link.label.padStart(maxLabelLength);
+    return `${chalk(paddedLabel)}: ${chalk.cyan.underline(link.url)}`;
+}).join('\n');
+const cardMessage = `${helloMessage}\n\n${linkMessages}`;
+const messageBox = boxen(cardMessage, {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'green',
+});
+console.log(messageBox);
